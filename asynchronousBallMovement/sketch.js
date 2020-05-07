0e3c04dcbe7b720583ee7ff01730c159dbbb74ff
shivaanshkanchan <61103461+shivaanshkanchan@users.noreply.github.com>
2020-05-07 14:01:31 +0530

    Add files via upload

---
+++ asynchronousBallMovement/sketch.js
@@ -0,0 +1,52 @@
+var syncball,database,position;
+
+function setup(){
+    database = firebase.database();
+    console.log(database);
+
+    createCanvas(500,500);
+    syncball = createSprite(250,250,10,10);
+    syncball.shapeColor = "red";
+
+    var syncballposition = database.ref('ball/position');
+    syncballposition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+
+   if(position !== undefined ) {
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+   }  
+}
+
+function readPosition(x,y){
+    position = data.val();
+    console.log(position.x);
+
+    syncball.x = position.x;
+    syncball.y = position.y;
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x': position.x + x, 
+        'y': position.y + y
+    })
+}
+
+function showError(){
+    console.log("Error in writing the function");
+}
